b91001ac1b59a0b5bb83f3b2f5464b53f8e53714
hashcatHitman <155700084+hashcatHitman@users.noreply.github.com>
2025-09-09 18:24:07 -0400

    chore(ch05): Apply formatting
    Signed-off-by: hashcatHitman <155700084+hashcatHitman@users.noreply.github.com>

--- mdbook/src/05-meet-your-software/examples/init.rs
+++ mdbook/src/05-meet-your-software/examples/init.rs
@@ -1,11 +1,9 @@
-#![deny(unsafe_code)]
 #![no_main]
 #![no_std]
 
 use cortex_m::asm;
 use cortex_m_rt::entry;
-use microbit as _;
-use panic_halt as _;
+use {microbit as _, panic_halt as _};
 
 #[entry]
 fn main() -> ! {
