53d9e48703931bd6655951bdc7aa47d9ffb46edf
hashcatHitman <155700084+hashcatHitman@users.noreply.github.com>
2025-09-09 18:22:27 -0400

    fix(ch05): Bring DelayNs in scope
    It just works.
    
    Signed-off-by: hashcatHitman <155700084+hashcatHitman@users.noreply.github.com>

--- mdbook/src/05-meet-your-software/examples/delay-print.rs
+++ mdbook/src/05-meet-your-software/examples/delay-print.rs
@@ -1,8 +1,8 @@
-#![deny(unsafe_code)]
 #![no_main]
 #![no_std]
 
 use cortex_m_rt::entry;
+use embedded_hal::delay::DelayNs;
 use microbit::board::Board;
 use microbit::hal::timer::Timer;
 use panic_rtt_target as _;
