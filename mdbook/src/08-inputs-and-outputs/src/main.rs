cab070fac7da05c564dd14f7f9bb910aa188310c
hashcatHitman <155700084+hashcatHitman@users.noreply.github.com>
2025-09-17 07:14:41 -0400

    feat(ch08): Turn Signal
    My solution for the chapter 8 challenge. Not particularly difficult.
    Spent more time messing around with const for fun than actually solving
    it.
    
    Signed-off-by: hashcatHitman <155700084+hashcatHitman@users.noreply.github.com>

--- mdbook/src/08-inputs-and-outputs/src/main.rs
+++ mdbook/src/08-inputs-and-outputs/src/main.rs
@@ -3,13 +3,74 @@
 
 use cortex_m_rt::entry;
 use embedded_hal::digital::InputPin;
-use microbit::board::Board;
+use microbit::Board;
 use microbit::display::blocking::Display;
-use microbit::hal::Timer;
+use microbit::hal::timer::Timer;
 use panic_rtt_target as _;
 use rtt_target::rtt_init_print;
 
+/// How long to display each frame for, in milliseconds.
+const FRAME_DURATION_MS: u32 = 10;
+
+/// Display a single lit LED in the center.
+const DISPLAY_CENTER: [[u8; 5]; 5] = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+];
+
+/// Display an arrow pointing left.
+const DISPLAY_LEFT: [[u8; 5]; 5] = [
+    [0, 0, 1, 0, 0],
+    [0, 1, 0, 0, 0],
+    [1, 1, 1, 1, 1],
+    [0, 1, 0, 0, 0],
+    [0, 0, 1, 0, 0],
+];
+
+/// Display an arrow pointing right.
+const DISPLAY_RIGHT: [[u8; 5]; 5] = [
+    [0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 0],
+    [1, 1, 1, 1, 1],
+    [0, 0, 0, 1, 0],
+    [0, 0, 1, 0, 0],
+];
+
 #[entry]
 fn main() -> ! {
-    loop {}
+    rtt_init_print!();
+
+    if let Some(board) = Board::take() {
+        let mut timer = Timer::new(board.TIMER0);
+        let mut display = Display::new(board.display_pins);
+
+        let mut button_a = board.buttons.button_a;
+        let mut button_b = board.buttons.button_b;
+
+        loop {
+            let (Ok(a_pressed), Ok(b_pressed)) =
+                (button_a.is_low(), button_b.is_low());
+            match (a_pressed, b_pressed) {
+                // Problem doesn't specify what to do if they're both pressed.
+                (true, true) => (),
+                (true, false) => {
+                    display.show(&mut timer, DISPLAY_LEFT, FRAME_DURATION_MS)
+                }
+                (false, true) => {
+                    display.show(&mut timer, DISPLAY_RIGHT, FRAME_DURATION_MS)
+                }
+                (false, false) => {
+                    display.show(&mut timer, DISPLAY_CENTER, FRAME_DURATION_MS)
+                }
+            }
+        }
+    } else {
+        #[expect(clippy::panic, reason = "Can't avoid it anymore.")]
+        {
+            panic!()
+        }
+    }
 }
