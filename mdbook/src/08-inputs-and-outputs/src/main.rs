539a84373739d6285185c1fe9b31399f4562f36f
hashcatHitman <155700084+hashcatHitman@users.noreply.github.com>
2025-09-16 21:03:28 -0400

    chore(ch08): Remove provided solution
    Seriously, why is this here already???
    
    Signed-off-by: hashcatHitman <155700084+hashcatHitman@users.noreply.github.com>

--- mdbook/src/08-inputs-and-outputs/src/main.rs
+++ mdbook/src/08-inputs-and-outputs/src/main.rs
@@ -3,52 +3,13 @@
 
 use cortex_m_rt::entry;
 use embedded_hal::digital::InputPin;
-use microbit::{board::Board, display::blocking::Display, hal::Timer};
+use microbit::board::Board;
+use microbit::display::blocking::Display;
+use microbit::hal::Timer;
 use panic_rtt_target as _;
 use rtt_target::rtt_init_print;
 
-// Define LED patterns
-const LEFT_ARROW: [[u8; 5]; 5] = [
-    [0, 0, 1, 0, 0],
-    [0, 1, 0, 0, 0],
-    [1, 1, 1, 1, 1],
-    [0, 1, 0, 0, 0],
-    [0, 0, 1, 0, 0],
-];
-
-const RIGHT_ARROW: [[u8; 5]; 5] = [
-    [0, 0, 1, 0, 0],
-    [0, 0, 0, 1, 0],
-    [1, 1, 1, 1, 1],
-    [0, 0, 0, 1, 0],
-    [0, 0, 1, 0, 0],
-];
-
-const CENTER_LED: [[u8; 5]; 5] = [
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 1, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-];
-
 #[entry]
 fn main() -> ! {
-    rtt_init_print!();
-    let board = Board::take().unwrap();
-    let mut timer = Timer::new(board.TIMER0);
-
-    let mut display = Display::new(board.display_pins);
-    let mut button_a = board.buttons.button_a;
-    let mut button_b = board.buttons.button_b;
-
-    loop {
-        if button_a.is_low().unwrap() {
-            display.show(&mut timer, LEFT_ARROW, 10);
-        } else if button_b.is_low().unwrap() {
-            display.show(&mut timer, RIGHT_ARROW, 10);
-        } else {
-            display.show(&mut timer, CENTER_LED, 10);
-        }
-    }
+    loop {}
 }
